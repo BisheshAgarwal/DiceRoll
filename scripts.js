07c5091a91cda797f6ce57db463c761514403989
BisheshAgarwal <52568056+BisheshAgarwal@users.noreply.github.com>
2020-04-02 18:54:25 +0530

    DIceRoll files

---
+++ scripts.js
@@ -0,0 +1,17 @@
+var randomPlayer1 = Math.floor(Math.random() * 6) + 1; 
+var randomPlayer2 = Math.floor(Math.random() * 6) + 1; 
+document.querySelector(".img1").setAttribute("src", "images/dice"+randomPlayer1+".png");
+document.querySelector(".img2").setAttribute("src", "images/dice"+randomPlayer2+".png");
+
+if(randomPlayer1 > randomPlayer2)
+    {
+        document.querySelector("h1").innerHTML = "Player 1 Wins";
+    }
+else if(randomPlayer1 === randomPlayer2)
+    {
+        document.querySelector("h1").innerHTML = "Draw";
+    }
+else
+    {
+        document.querySelector("h1").innerHTML = "Player 2 Wins";
+    }
